eba33a4646df38fef9289c63cd9c2e7fe66b542b
Taylor Council <councit@gmail.com>
2020-06-01 13:23:35 -0700

    reading writing to files

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,23 @@
 # pcost.py
 #
 # Exercise 1.27
+
+with open('Data/portfolio.csv', 'rt') as f:
+	headers = next(f).split(',')
+	total_cost = 0.0
+
+	for line in f:
+		row = line.split(',')
+
+		num1 = float(row[1])
+		num2 = float(row[-1])
+		total_cost += num1 * num2
+
+	print(f'Total cost {total_cost}')
+
+
+
+
+
+
+
